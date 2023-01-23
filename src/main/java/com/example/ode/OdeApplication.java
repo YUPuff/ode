cce3cbe10cda80292a1ec57721ce0490ff32a3ea
YUPuff <362834277@qq.com>
2023-01-23 16:45:08 +0800

    Initial commit

---
+++ src/main/java/com/example/ode/OdeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ode/OdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
